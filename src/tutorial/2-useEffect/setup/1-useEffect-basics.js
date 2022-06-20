2dbe5a736c630fa6c793e5515a1f5a30d384eb97
arborin <nika.kobaidze@gmail.com>
2022-06-20 23:43:30 +0400

    [ADD] useEffect

--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -3,7 +3,22 @@ import React, { useState, useEffect } from 'react';
 // cleanup function
 // second parameter
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  
+  const [value, setValue] = useState(0)
+  
+  useEffect(()=>{
+    console.log('Call useEffect')
+    if(value > 0){
+      
+      document.title = `New (${value})`;
+    }
+  })
+  
+  
+  return (<>
+    <h1>{value}</h1>
+    <button className='btn' onClick={()=>setValue(value + 1)}>OK</button>
+  </>)
 };
 
 export default UseEffectBasics;
