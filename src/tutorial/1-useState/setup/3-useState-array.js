e13d5fa1b5a85b57baf196f651d7a75cac156cc5
nkobaidze <nika.kobaidze@gmail.com>
2022-06-20 15:11:30 +0400

    [ADD] useState

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { data } from '../../../data';
 
 const UseStateArray = () => {
+  // WITHOUT IMPORT
+  const [people, setPeople] = React.useState([])
   return <h2>useState array example</h2>;
 };
 
