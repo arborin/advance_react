a97b1f9a28661fe5d5174ef5fb817de08e3ed087
nkobaidze <nika.kobaidze@gmail.com>
2022-06-20 15:45:25 +0400

    [ADD] filter values

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,8 +3,30 @@ import { data } from '../../../data';
 
 const UseStateArray = () => {
   // WITHOUT IMPORT
-  const [people, setPeople] = React.useState([])
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = React.useState(data)
+  
+  console.log(people)
+  
+  const removeItem = (id) => {
+    let newPeople = people.filter((person)=>person.id !== id)
+    
+    setPeople(newPeople)
+  }
+  
+  return (<div>
+          {
+            people.map((person) => {
+              const {id, name} = person;
+              console.log(person)
+              
+              return (<div key={id} className='item'>
+                          {name} <button className='' onClick={() => removeItem(id)}>Remove</button>
+                      </div>)
+              
+            }) 
+          }
+          
+        </div>)
 };
 
 export default UseStateArray;
