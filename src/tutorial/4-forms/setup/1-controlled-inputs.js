5a4b4c2a291ad635c512eb7b98c7512f6e79c4f2
arborin <nika.kobaidze@gmail.com>
2022-06-24 00:11:41 +0400

    [ADD] handle form inputs

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -6,21 +6,32 @@ import React, { useState } from 'react';
 // value, onChange
 
 const ControlledInputs = () => {
-  return (
-    <article>
-      <form className='form'>
-        <div className='form-control'>
-          <label htmlFor='firstName'>Name:</label>
-          <input type='text' id='firstName' name='firstName'/>
-        </div>
-        <div className='form-control'>
-          <label htmlFor='email'>Email:</label>
-          <input type='text' id='email' name='email'/>
-        </div>
-        <button type='submit'>Add Person</button>
-      </form>
-    </article>
-  )
+    
+    const [firstName, setFirstName] = useState('');
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(firstName);
+        console.log(email);
+    }
+
+
+    return (
+        <article>
+            <form className='form' onSubmit={handleSubmit}>
+                <div className='form-control'>
+                    <label htmlFor='firstName'>Name:</label>
+                    <input type='text' id='firstName' name='firstName' value={firstName} onChange={(e) => setFirstName(e.target.value)}/>
+                </div>
+                <div className='form-control'>
+                    <label htmlFor='email'>Email:</label>
+                    <input type='text' id='email' name='email' value={email} onChange={(e) => setEmail(e.target.value)}/>
+                </div>
+                <button type='submit'>Add Person</button>
+            </form>
+        </article>
+    )
 };
 
 export default ControlledInputs;
