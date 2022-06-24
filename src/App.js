dda28db6f890b8fd1d42dbd9c55378184a29534e
nkobaidze <nika.kobaidze@gmail.com>
2022-06-24 14:22:38 +0400

    [123]

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Setup from './tutorial/2-useEffect/setup/3-useEffect-fetch-data'
+import Setup from './tutorial/3-conditional-rendering/setup/1-multiple-returns'
 
 
 function App() {
@@ -11,3 +11,5 @@ function App() {
 }
 
 export default App
+
+
